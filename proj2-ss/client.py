71e170948acde4c5651cac1fba2e5f4e18e06285
Matthew Jallouk <jalloukmat@yahoo.com>
2015-11-25 15:28:58 -0500

    Updated code to pass in parameters and solidify communication between client and server

--- proj2-ss/client.py
+++ proj2-ss/client.py
@@ -5,13 +5,15 @@ import argparse
 
 #Supress warnings when checking common name of CA cert
 requests.packages.urllib3.disable_warnings()
-
+defaultArg='NO_ARG_PASSED'
 #Create parser object
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(argument_default=defaultArg)
 
 #Create parser arguments for checking, we store whatever value is passed to them.
-parser.add_argument('--check_in', action='store')
-parser.add_argument('--client', action='store')
+parser.add_argument('--check_in', action='store', nargs=2)
+parser.add_argument('--check_out', action='store')
+parser.add_argument('--delegate', action='store')
+parser.add_argument('--client', action='store', required=True)
 
 #Parse the args and place them in a var. We create the directory paths to wherever our certs and keys are for referencing.
 args = parser.parse_args();
@@ -20,6 +22,16 @@ server_crt = os.getcwd() + '/server/securesysServer.crt'
 client_crt = os.getcwd() + '/clients/' + args.client + '/securesysClient.crt'
 client_key = os.getcwd() + '/clients/' + args.client + '/securesysClient.key'
 
+if args.check_in != defaultArg:
+    r = requests.get("http://localhost:5000/check_in", verify = cert_auth_crt, params=data)
+elif args.check_out != defaultArg:
+    print('arg checkout')
+elif args.delegate != defaultArg:
+    pring('arg delegate')
+    
+
 data={'client':args.client,'check_in':args.check_in}
-r = requests.get("https://localhost/", verify = cert_auth_crt, params=data)
+#r = requests.get("https://localhost/", verify = cert_auth_crt, params=data)
+
 print(r.text)
+
--- proj2-ss/server.py
+++ proj2-ss/server.py
@@ -1,19 +1,19 @@
 from flask import Flask
+from flask import Flask, request
+import os
+import sys
+import requests
 app = Flask(__name__)
 
-@app.route("/checkin")
-def checkin():
-    name = request.args.get('name')
-    fileName = request.files('file')
-    return 'Hello World'
+@app.route("/check_in")
+def check_in():
+    check_in = request.args.get('check_in')
+    client = request.args.get('client')    
+    return client+''
 
-@app.route("/checkout")
+@app.route("/check_out")
 def checkout():
-    return 'checkout'
-
-@app.route("/")
-def index():
-    return 'index'
+    return 'check_out'
 
 if __name__ == '__main__':
     app.run()
