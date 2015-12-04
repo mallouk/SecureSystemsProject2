1a9374c9dc5589c1dae0f03b5f427139134dd5a5
Matthew Jallouk <jalloukmat@yahoo.com>
2015-12-04 13:39:42 -0500

    Updated run cases

--- proj2-ss/verify_signature.py
+++ proj2-ss/verify_signature.py
@@ -19,16 +19,26 @@ serverDir = '/home/matthew/git/SecureSystemsProject2/proj2-ss/server/files/'
 if not os.path.isfile(serverDir + sys.argv[1]):
     print "File doesn't exist, try again please."
 else:
-    with open(serverDir + '.' + sys.argv[1] + '.sign', 'r') as metafile:
-        first_line = metafile.readline().replace('\n','')
-        parsed = first_line.split('***')
+    if os.path.isfile(serverDir + '.' + sys.argv[1] + '.sign'):
+        with open(serverDir + '.' + sys.argv[1] + '.sign', 'r') as metafile:
+            first_line = metafile.readline().replace('\n','')
+            parsed = first_line.split('***')
         
-        if '.enc.sign' in sys.argv[1]:
-            signVer = verify_signature(serverDir, sys.argv[1], parsed[2])
-        else:
             signVer = verify_signature(serverDir, sys.argv[1], parsed[1])
 
-        if signVer:
-           print "Signature verified and is correct"
-        else:
-            print "Signature not correct, file has been tampered in some way"
+            if signVer:
+                print "Signature verified and is correct"
+            else:
+                print "Signature not correct, file has been tampered in some way"
+    else:
+        with open(serverDir + '.' + sys.argv[1] + '.enc.sign', 'r') as metafile:
+            first_line = metafile.readline().replace('\n','')
+            parsed = first_line.split('***')
+        
+            signVer = verify_signature(serverDir, sys.argv[1], parsed[2])
+                            
+            if signVer:
+                print "Signature verified and is correct"
+            else:
+                print "Signature not correct, file has been tampered in some way"
+
