71977913aaadf239cb109e80fac48b99efd8351a
Matthew Jallouk <jalloukmat@yahoo.com>
2015-12-03 23:02:11 -0500

    Creating script for run cases

--- proj2-ss/verify_signature.py
+++ proj2-ss/verify_signature.py
@@ -18,10 +18,8 @@ first_line=''
 serverDir = '/home/matthew/git/SecureSystemsProject2/proj2-ss/server/files/'
 if not os.path.isfile(serverDir + sys.argv[1]):
     print "File doesn't exist, try again please."
-elif not '.sign' in sys.argv[1]:
-    print "You must give a signed file in order for us to verify the file's signature"
 else:
-    with open(serverDir + '.' + sys.argv[1], 'r') as metafile:
+    with open(serverDir + '.' + sys.argv[1] + '.sign', 'r') as metafile:
         first_line = metafile.readline().replace('\n','')
         parsed = first_line.split('***')
         
