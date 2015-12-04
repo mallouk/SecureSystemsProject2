2cddffc011bf3fea292d87afd93171af6ee20fb4
Tito Nieves <Tito.Nieves@gmail.com>
2015-12-04 14:32:58 -0500

    Update install.sh
    Cleaned up comments.

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ sudo apt-get -y install git gcc g++ make gedit nginx openssl
 sudo apt-get -y install python python-virtualenv
 sudo apt-get -y install python-dev
 sudo apt-get -y install python-pip
+
 pip install pycrypto
 pip install flask
 
@@ -16,12 +17,11 @@ echo ""
 echo ""
 echo "gcc/g++/make...for general dev tools and if you ever want to enable VBoxGuestAdditions"
 echo "gedit...for your developing purposes"
-echo "emacs...in case you ever want to get into a wonderful thing of emacs text editing"
-echo "python...again obvious"
+echo "python"
 echo "python-virtualenv...used with flask"
 echo "nginx...used as our server backend"
-echo "openssl...cert things"
+echo "openssl...certificate tools"
 echo "python-pip and python-dev"
-echo "install pycrypto"
+echo "pycrypto for hashing and encrypting"
 echo ""
 echo "I believe that is everything. Thanks!"
