910b5d016828a746ba6b5424763cb9ac3a1edfa3
Tito Nieves <Tito.Nieves@gmail.com>
2015-12-04 19:34:07 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
-sudo apt-get -y install git gcc g++ make gedit nginx openssl
+sudo apt-get -y install git gcc g++ 
+sudo apt-get -y install openssl
+sudo apt-get -y install nginx
 sudo apt-get -y install python python-virtualenv
 sudo apt-get -y install python-dev
 sudo apt-get -y install python-pip
@@ -15,8 +17,7 @@ git clone https://github.com/mallouk/SecureSystemsProject2.git
 echo ""
 echo ""
 echo ""
-echo "gcc/g++/make...for general dev tools and if you ever want to enable VBoxGuestAdditions"
-echo "gedit...for your developing purposes"
+echo "gcc and g++...for general dev tools and if you ever want to enable VBoxGuestAdditions"
 echo "python"
 echo "python-virtualenv...used with flask"
 echo "nginx...used as our server backend"
